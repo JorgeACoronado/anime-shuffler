b1a2bcee98f3eace32aca1129c76b32e51482793
Jorge Coronado <60112119+JorgeACoronado@users.noreply.github.com>
2026-03-08 13:12:56 -0500

    Added VueUse and worked on header

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,5 +3,15 @@
 </script>
 
 <template>
-  <main class="flex flex-1 items-center justify-center">Hello There</main>
+  <main class="min-h-screen bg-[#0F172A] px-4 py-8 text-white sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl">
+      <header>
+        <p class="text-xs font-semibold tracking-[0.3em] text-violet-400 uppercase">Project #4</p>
+        <h1 class="mt-2 text-4xl font-black text-[#F8FAFC] sm:text-5xl">Anime Shuffler</h1>
+        <p class="mt-2 max-w-3xl text-sm text-[#F8FAFC]/80">Spin the reel, request a random anime from Jinkan with VueUse useFech, 
+          and learn how REST API's signal rate limiting with HTTP 429.
+        </p>
+      </header>
+    </div>
+  </main>
 </template>
